1464c7761730388c92e7de44c9bc634857fe7b9d
GTheD01 <popeftimovgeorgi@yahoo.com>
2024-11-20 17:27:31 +0100

    Dynamically displaying the selected question with answers

--- frontend/src/components/Pagination.tsx
+++ frontend/src/components/Pagination.tsx
@@ -1,14 +1,22 @@
-import { useState } from "react";
+import { Dispatch, SetStateAction } from "react";
 
-export default function Pagination() {
-  const [activeQuestion, setActiveQuestion] = useState<number | null>(1);
+type PaginationProps = {
+  selectedQuestion: number;
+  setSelectedQuestion: Dispatch<SetStateAction<number>>;
+};
+
+export default function Pagination({
+  selectedQuestion,
+  setSelectedQuestion,
+}: PaginationProps) {
   return (
     <div className="flex justify-around">
       {Array.from({ length: 10 }, (_, index) => index + 1).map((question) => (
         <div
-          onClick={() => setActiveQuestion(question)}
+          key={question}
+          onClick={() => setSelectedQuestion(question)}
           className={`text-[var(--primary-color)] rounded-full w-10 h-10 cursor-pointer flex items-center justify-center ${
-            activeQuestion === question ? "bg-blue-300" : "bg-white"
+            selectedQuestion === question ? "bg-blue-300" : "bg-white"
           }`}
         >
           {question}
--- frontend/src/pages/QuizResults.tsx
+++ frontend/src/pages/QuizResults.tsx
@@ -1,47 +1,48 @@
+import { useState } from "react";
 import Answer from "../components/Answer";
 import Pagination from "../components/Pagination";
+import { useAppSelector } from "../redux/hooks";
+import { Link } from "react-router-dom";
 
 export default function QuizResults() {
+  const [selectedQuestion, setSelectedQuestion] = useState(1);
+  const quizTopic = useAppSelector((store) => store.quiz.topic);
+  const userAnswers = useAppSelector((store) => store.quiz.userAnswers);
+  const questions = useAppSelector((store) => store.quiz.questions);
+  const question = questions[selectedQuestion - 1];
+  let answers: string[] = [];
+  if (question) {
+    answers = [...question.incorrect_answers, question.correct_answer];
+  }
+
   return (
     <div className="flex items-center justify-center flex-col px-12">
-      <h2 className="text-4xl mb-8">Quiz Topic:</h2>
-      <div className="px-20 py-12 shadow-2xl space-y-6 max-w-[768px] min-w-[330px]">
-        <Pagination />
-        <h3 className="text-4xl text-blue-500 font-bold">Question 1</h3>
-        <h5 className="text-3xl tracking-wide">
-          Which of the following is OOP language?
-        </h5>
+      <h2 className="text-4xl mb-8">Quiz Topic: {quizTopic}</h2>
+      <div className="px-20 py-12 shadow-2xl space-y-6 md:w-[650px] w-[450px]">
+        <Pagination
+          selectedQuestion={selectedQuestion}
+          setSelectedQuestion={setSelectedQuestion}
+        />
+        <h3 className="text-4xl text-blue-500 font-bold">
+          Question {selectedQuestion}
+        </h3>
+        <h5 className="text-3xl tracking-wide">{question.question}</h5>
         <div className="space-y-2">
-          <Answer
-            value="Java"
-            id="Java"
-            name="answer"
-            selectedAnswer={"C"}
-            correctAnswer="Cobol"
-          />
-          <Answer
-            value="Cobol"
-            id="Cobol"
-            name="answer"
-            selectedAnswer={"C"}
-            correctAnswer="Cobol"
-          />
-          <Answer
-            value="C"
-            id="C"
-            name="answer"
-            selectedAnswer={"C"}
-            correctAnswer="Cobol"
-          />
-          <Answer
-            value="Assembly"
-            id="Assembly"
-            name="answer"
-            selectedAnswer={"C"}
-            correctAnswer="Cobol"
-          />
+          {answers?.map((answer) => (
+            <Answer
+              key={answer}
+              value={answer}
+              id={answer}
+              name="answer"
+              selectedAnswer={userAnswers[selectedQuestion - 1]}
+              correctAnswer={question?.correct_answer}
+            />
+          ))}
         </div>
       </div>
+      <Link className="bg-blue-400 p-4 hover:bg-blue-500" to={"/"}>
+        Play Again
+      </Link>
     </div>
   );
 }
